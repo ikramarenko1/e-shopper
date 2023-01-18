46eaa2d04b2d75e61dfa8e617ea00b5144d861a0
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-01-19 00:13:18 +0200

    added home swiper

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Initialize Home Swiper
+var homeSwiper = new Swiper(".home-swiper", {
+	spaceBetween: 30,
+	loop: "true",
+
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+ });
