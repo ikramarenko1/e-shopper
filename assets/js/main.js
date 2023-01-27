dce98c0573ef8b377762d42b55ed08760c575cb3
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-01-27 08:14:06 +0200

    finished faq's-page styles and added accordion script

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,34 @@ function scrollUp() {
 		scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener('scroll', scrollUp);
+
+
+// Questions Accordion
+const accordionItem = document.querySelectorAll(".questions__item");
+
+accordionItem.forEach((item) => {
+	const accordionHeader = item.querySelector(".questions__header");
+
+	accordionHeader.addEventListener("click", () => {
+		const openItem = document.querySelector(".accordion-open");
+
+		toggleItem(item);
+
+		if (openItem && openItem !== item) {
+			toggleItem(openItem)
+		}
+	})
+})
+
+const toggleItem = (item) => {
+	const accordionContent = item.querySelector('.questions__content');
+	const accordionIcon = item.querySelector(".questions__icon");
+
+	if (item.classList.contains("accordion-open")) {
+		accordionContent.removeAttribute("style");
+		item.classList.remove("accordion-open");
+	} else {
+		accordionContent.style.height = accordionContent.scrollHeight + "px";
+		item.classList.add("accordion-open");
+	}
+}
