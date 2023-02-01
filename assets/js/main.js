cc91c5e118912b47fcf2dacfd52fce463abcd7bc
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-02-01 14:43:20 +0200

    fixed some comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,14 +42,14 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-// Показать корзину
+// Показать меню
 if (navToggle) {
 	navToggle.addEventListener("click", () => {
 		navMenu.classList.add("show-menu");
 	})
 }
 
-// Спрятать корзину
+// Спрятать меню
 if (navClose) {
 	navClose.addEventListener("click", () => {
 		navMenu.classList.remove("show-menu");
